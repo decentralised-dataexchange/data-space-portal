2ec6d565948567e4d4e8fe86e43e6c5130bfeb26
karthik cs <karthikcs@karthiks-MacBook-Pro.local>
2024-03-05 22:41:30 +0530

    Initial React boiler plate with login screen

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/ban-types */
+import React from 'react';
+import AppRouter from './routes';
+import './index.css';
+
+const App = () => {
+  return <AppRouter />
+};
+
+export default App;
