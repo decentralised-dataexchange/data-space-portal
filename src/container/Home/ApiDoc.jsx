c34dc79440543807252ffdb9f8d1d6df74edb3e3
George J Padayatti <george.padayatti@igrant.io>
2024-04-07 19:29:00 +0530

    Fix: Update rapi-doc layout and style
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/container/Home/ApiDoc.jsx
+++ src/container/Home/ApiDoc.jsx
@@ -8,8 +8,23 @@ const ApiDoc = () => {
     return(
         <rapi-doc
           spec-url = {location?.state}
-          render-style = "read"
+          render-style = "view"
           style = {{ height: "100vh", width: "100%" }}
+          theme="light"
+          persist-auth="true"
+          show-info="true"
+          show-header="false"
+          nav-bg-color="#00182C"
+          nav-text-color="#fff"
+          primary-color="#00182C"
+          show-method-in-nav-bar="as-colored-block"
+          show-components="false"
+          allow-spec-file-download="false"
+          show-curl-before-try="false"
+          schema-style="table"
+          font-size="large"
+          schema-expand-level="1"
+          default-schema-tab="schema"
         >
         </rapi-doc>
     )
