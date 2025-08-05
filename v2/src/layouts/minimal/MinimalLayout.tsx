ca94531dd8599b0e170707437616f1742211114e
Mylavarapu Sarath Sai <msarathsai@proton.me>
2025-08-05 12:48:29 +0530

    Fix: Hiding navbar in the login route

--- v2/src/layouts/minimal/MinimalLayout.tsx
+++ v2/src/layouts/minimal/MinimalLayout.tsx
@@ -3,12 +3,14 @@ import MinimalAppBar from '@/components/common/AppBar/MinimalAppBar';
 import Box from '@mui/material/Box';
 import Footer from '@/components/common/Footer';
 import '../style.scss';
+import { usePathname } from 'next/navigation';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-
+  const pathname = usePathname();
+  const isLoginRoute = pathname.includes('/login');
   return (
     <>
-      <MinimalAppBar />
+      {isLoginRoute ? null : <MinimalAppBar />}
       <Box className="leftNavigationContainer">
         <Box component="main">
           {children}
