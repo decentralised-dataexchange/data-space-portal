63ed3eb3dd26e76730f2b6448339e9f284b8db26
Mylavarapu Sarath Sai <msarathsai@proton.me>
2025-08-17 15:23:56 +0530

    Login page footer going under fold fixed

--- v2/src/layouts/minimal/MinimalLayout.tsx
+++ v2/src/layouts/minimal/MinimalLayout.tsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Footer from '@/components/common/Footer';
 import '../style.scss';
 import { usePathname } from 'next/navigation';
+import Breadcrumb from '@/components/common/Breadcrumb';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
@@ -12,7 +13,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <>
       {isLoginRoute ? null : <MinimalAppBar />}
       <Box className="leftNavigationContainer">
-        <Box component="main">
+        <Box component="main" sx={{ paddingTop: isLoginRoute ? 0 : '80px', flex: 1 }}>
+          {!isLoginRoute && <Breadcrumb />}
           {children}
         </Box>
         <Box className="footerContainer d-flex-center">
